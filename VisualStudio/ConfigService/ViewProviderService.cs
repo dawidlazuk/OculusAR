4d0739b10b688274772b318b2d77f665d0f342d4
Dawid Lazuk <dawid.lazuk@gmail.com>
2017-11-26 21:07:44 +0100

    Config service draft

---
+++ VisualStudio/ConfigService/ViewProviderService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using ViewProvision.Contract;
+
+namespace ConfigService
+{
+    class ViewProviderService : IViewProvider
+    {
+        private IViewProvider viewProvider;
+
+        public ViewProviderService(IViewProvider viewProvider)
+        {
+            this.viewProvider = viewProvider;
+        }
+
+#region IViewProvider implementation
+
+        public IEnumerable<int> GetAvailableCaptureIndexes()
+        {
+            return viewProvider.GetAvailableCaptureIndexes();
+        }
+
+        public ViewData GetCurrentView()
+        {
+            return viewProvider.GetCurrentView();
+        }
+
+        public ViewDataInternal GetCurrentViewInternal()
+        {
+            return viewProvider.GetCurrentViewInternal();
+        }
+
+        public void RotateImage(CaptureSide captureSide, RotateSide rotateSide)
+        {
+            viewProvider.RotateImage(captureSide, rotateSide);
+        }
+
+        public void SetCapture(CaptureSide captureSide, int cameraIndex)
+        {
+            viewProvider.SetCapture(captureSide, cameraIndex);
+        }
+
+        public void UpdateFrames()
+        {
+            viewProvider.UpdateFrames();
+        }
+
+#endregion
+    }
+}
