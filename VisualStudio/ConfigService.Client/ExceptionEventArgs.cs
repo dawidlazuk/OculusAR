618e0ce7ecc333c02c144e84a8491bb68caa4706
Dawid Lazuk <daw>
2017-12-02 21:21:34 +0100

    service client exceptions handling

---
+++ VisualStudio/ConfigService.Client/ExceptionEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ConfigService.Client
+{
+    public delegate void ExceptionEventHandler(object sender, ExceptionEventArgs e);
+
+    public class ExceptionEventArgs : EventArgs
+    {
+        public Exception Exception { get; set; }
+    }
+}
