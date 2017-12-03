134a13a68260c7214f358a0b3b3d4af919715ac5
Dawid Lazuk <daw>
2017-12-03 16:59:35 +0100

    Unity updates frames

--- Unity/DoubleEye/Assets/Transmitter/StereoVidTransmitter.cs
+++ Unity/DoubleEye/Assets/Transmitter/StereoVidTransmitter.cs
@@ -15,6 +15,7 @@ namespace Assets.Transmitter
 
         public StereoView GetStereoView()
         {
+            _viewProvider.UpdateFrames();
             var currentView = _viewProvider.GetCurrentView();
 
             return new StereoView()
