5be4686ac637a4dc7fcd085cc46c74a15e4c5e9f
Piciupol <piottrus10@gmail.com>
2017-12-03 03:10:44 +0100

    Parallel texture converting

--- Unity/DoubleEye/Assets/Transmitter/StereoVidTransmitter.cs
+++ Unity/DoubleEye/Assets/Transmitter/StereoVidTransmitter.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using UnityEngine;
 using ViewProvision.Contract;
 
 namespace Assets.Transmitter
@@ -7,21 +10,74 @@ namespace Assets.Transmitter
         private readonly ITextureConverter _textureConverter;
         private readonly IViewProvider _viewProvider;
 
+        private ViewDataImage _currentView;
+
+        private Texture _leftTexture, _rightTexture;
+        private Thread[] _threads;
+
+        private Action[] _threadActions;
+        private ManualResetEvent[] _startEvents;
+        private ManualResetEvent[] _finishEvents;
+
         public StereoVidTransmitter(ITextureConverter textureConverter, IViewProvider viewProvider)
         {
             _textureConverter = textureConverter;
             _viewProvider = viewProvider;
+
+            InitializeThreadsAndEvents();
         }
 
         public StereoView GetStereoView()
         {
-            var currentView = _viewProvider.GetCurrentView();
+            _currentView = _viewProvider.GetCurrentView();
+
+            ConvertImageToTexturesInSeparateThreads();
 
             return new StereoView()
             {
-                LeftEye = _textureConverter.FromImage(currentView.LeftImage),
-                RightEye = _textureConverter.FromImage(currentView.RightImage)
+                LeftEye = _leftTexture,
+                RightEye = _rightTexture
             };
         }
+
+        private void ConvertImageToTexturesInSeparateThreads()
+        {
+            foreach (var startEvent in _startEvents)
+                startEvent.Set();
+
+            WaitHandle.WaitAll(_finishEvents);
+
+            foreach (var finishEvent in _finishEvents)
+                finishEvent.Reset();
+        }
+
+        private void InitializeThreadsAndEvents()
+        {
+            _threads[0] = new Thread(() => SetTexture(0));
+            _threads[1] = new Thread(() => SetTexture(1));
+
+            _startEvents = new ManualResetEvent[2];
+            _finishEvents = new ManualResetEvent[2];
+
+            _threadActions = new Action[]
+            {
+                () => _leftTexture = _textureConverter.FromImage(_currentView.LeftImage),
+                () => _rightTexture = _textureConverter.FromImage(_currentView.RightImage)
+            };
+        }
+
+        private void SetTexture(int side)
+        {
+            while (true)
+            {
+                _startEvents[side].WaitOne();
+
+                _threadActions[side].Invoke();
+
+                _startEvents[side].Reset();
+                _finishEvents[side].Set();
+            }
+        }
+
     }
 }
